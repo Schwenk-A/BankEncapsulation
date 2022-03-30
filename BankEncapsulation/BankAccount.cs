051cd471cd50825df2d290d2af8c74cb1385629c
Andrea Schwenk <andreaschwenk3@gmail.com>
2022-03-30 14:22:05 -0500

    Completed project

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+
+        private double Balance = 0;
+
+        public double Deposit(double amountDeposit)
+        {
+           return Balance += amountDeposit;
+        }
+
+        public double GetBalance()
+        {
+            return Balance;
+        }
+
+
+
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+                var bankAccount = new BankAccount();
+            bankAccount.Deposit(5);
+            Console.WriteLine(bankAccount.GetBalance());
+            bankAccount.Deposit(3);
+            Console.WriteLine(bankAccount.GetBalance());
+
+        }
+    }
+}
